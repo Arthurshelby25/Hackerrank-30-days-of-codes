6ec6cf7ff617f9e6c157295fba03398374c5c2cf
Arthurshelby25 <transportemc30@gmail.com>
2026-07-09 22:34:00 +0000

    est es mi reto del dia 1 en python

---
+++ HackerRank/Day1/Day1.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+usuario_i = int(input())
+usuario_d = float(input())
+usuario_s = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + usuario_i)
+
+# Print the sum of the double variables on a new line.
+print(d + usuario_d)
+
+# Concatenate and print the String variables on a new line
+print(s + usuario_s)
